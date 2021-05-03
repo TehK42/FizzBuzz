2e5d2381aa0ba0b2161f2dd0769bda92cf635cbc
Luigi <tupazluisgabriel@gmail.com>
2021-05-03 13:43:37 +0800

    "added method solution"

--- src/com/tehk42/Main.java
+++ src/com/tehk42/Main.java
@@ -3,8 +3,8 @@ package com.tehk42;
 public class Main {
 
     public static void main(String[] args) {
-        //for loop solution
-        for(int i = 0; i < 101; i++) {
+//        //for loop solution
+        for(int i = 1; i <= 100; i++) {
             if(i % 15 == 0) {
                 System.out.println("FIZZBUZZ");
             } else if(i % 5 == 0) {
@@ -16,34 +16,51 @@ public class Main {
             }
         }
         System.out.println("\n");
-
-        int i = 0;
-        //while solution
-//        while(i < 100) {
-//            i++;
-//            if(i % 15 == 0) {
+        fizzBuzz(3,5,15);
+//
+//        int i = 0;
+//        //while solution
+////        while(i < 100) {
+////            i++;
+////            if(i % 15 == 0) {
+////                System.out.println("FIZZBUZZ");
+////            } else if(i % 5 == 0) {
+////                System.out.println("BUZZ");
+////            } else if(i % 3 == 0) {
+////                System.out.println("FIZZ");
+////            } else {
+////                System.out.println(i);
+////            }
+////        }
+//
+//        //do while solution
+//        do {
+//            if (i % 15 == 0) {
 //                System.out.println("FIZZBUZZ");
-//            } else if(i % 5 == 0) {
+//            } else if (i % 5 == 0) {
 //                System.out.println("BUZZ");
-//            } else if(i % 3 == 0) {
+//            } else if (i % 3 == 0) {
 //                System.out.println("FIZZ");
 //            } else {
 //                System.out.println(i);
 //            }
-//        }
-
-        //do while solution
-        do {
-            if (i % 15 == 0) {
+//            i++;
+//        } while(i < 101);
+//
+//        System.out.println("\r");
+    }
+    //method solution with interchangeable logic values
+    public static void fizzBuzz(int x, int y , int z) {
+        for(int i = 1; i <= 100; i++) {
+            if(i % z == 0) {
                 System.out.println("FIZZBUZZ");
-            } else if (i % 5 == 0) {
+            } else if(i % y == 0) {
                 System.out.println("BUZZ");
-            } else if (i % 3 == 0) {
+            } else if(i % x == 0) {
                 System.out.println("FIZZ");
             } else {
                 System.out.println(i);
             }
-            i++;
-        } while(i < 101);
+        }
     }
 }
